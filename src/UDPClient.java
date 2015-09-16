948957dfd9558671650b27251dd515c5f9c84345
Yitar <w.yitar@gmail.com>
2015-09-16 16:15:02 +0800

    v1.0

---
+++ src/UDPClient.java
@@ -0,0 +1,13 @@
+import java.net.*;
+import java.io.*;
+
+public class UDPClient {
+	public static void main(String[] args) throws Exception {
+		byte buf[] = (new String("Hello")).getBytes();
+		DatagramPacket dp = new DatagramPacket(buf, buf.length, new InetSocketAddress("127.0.0.1", 5678));
+		DatagramSocket ds = new DatagramSocket(9999);
+		ds.send(dp);
+		ds.close();
+	}
+	
+}
